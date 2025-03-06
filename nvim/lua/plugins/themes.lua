ca28d45d7caa5fbba6a3d260d0b92349d300aee5
Колоколов Глеб Игоревич <kgi23u917@student.bmstu.ru>
2025-03-06 17:05:50 +0300

    Neovim: improve theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -15,22 +15,43 @@ return {
 		"maxmx03/fluoromachine.nvim",
 		lazy = true,
 		config = function()
+			local custom_first = {
+				comments = { italic = true, fg = "#8c7aa9" },
+				functions = { fg = "#b388eb" },
+				variables = {},
+				numbers = {},
+				constants = { bold = true, fg = "#fa198b" }, -- #60b2e5
+				parameters = { fg = "#8093f1" },
+				keywords = { fg = "#60b2e5" },
+				types = { fg = "#fa43a1" },
+			}
+
+			local custom_second = {
+				comments = { italic = true },
+				functions = {},
+				variables = {},
+				numbers = {},
+				constants = { bold = true },
+				parameters = { fg = "#8ae9c1" },
+				keywords = { fg = "#087e8b" },
+				types = { fg = "#ff5a5f" },
+			}
+
 			require("fluoromachine").setup({
 				theme = "retrowave",
 				brightness = 0.05,
 				glow = true,
 				transparent = false,
-				styles = {
-					comments = {},
-					functions = {},
-					variables = {},
-					numbers = {},
-					constants = { bold = true },
-					parameters = {},
-					keywords = {},
-					types = {},
-				},
-				colors = {},
+				styles = custom_first,
+				colors = function(_, color)
+					local darken = color.darken
+					return {
+						orange = "#436ee1",
+						yellow = "#acadfe",
+						purple = "#fa189b",
+						red = "#fa43a1",
+					}
+				end,
 				overrides = {},
 				plugins = {
 					bufferline = true,
