a1cf40ac5647f3e7ea453bbd2e30b824589ab844
Колоколов Глеб Игоревич <kgi23u917@student.bmstu.ru>
2025-04-03 22:52:39 +0300

    Neovim: lsp after and proper nvim-java setup

--- nvim/after/ftplugin/java.lua
+++ nvim/after/ftplugin/java.lua
@@ -3,9 +3,50 @@ if not vim.g.java_setup then
 
 	local java = require("java")
 
-	java.setup({})
+	java.setup({
+		root_markers = {
+			"settings.gradle",
+			"settings.gradle.kts",
+			"pom.xml",
+			"build.gradle",
+			"mvnw",
+			"gradlew",
+			"build.gradle",
+			"build.gradle.kts",
+			".git",
+		},
 
-	require("lspconfig").jdtls.setup({})
+		jdtls = {
+			version = "v1.43.0",
+		},
+
+		lombok = {
+			enable = true,
+			version = "nightly",
+		},
+
+		-- load java test plugins
+		java_test = {
+			enable = true,
+			version = "0.40.1",
+		},
+
+		-- load java debugger plugins
+		java_debug_adapter = {
+			enable = true,
+			version = "0.58.1",
+		},
+
+		spring_boot_tools = {
+			enable = false,
+			version = "1.55.1",
+		},
+
+		notifications = {
+			-- enable 'Configuring DAP' & 'DAP configured' messages on start up
+			dap = false,
+		},
+	})
 
 	vim.api.nvim_create_autocmd("LspAttach", {
 		group = vim.api.nvim_create_augroup("JavaLspAttach", { clear = true }),
--- nvim/lua/plugins/nvim-java.lua
+++ nvim/lua/plugins/nvim-java.lua
@@ -1,48 +1,6 @@
 return {
 	"nvim-java/nvim-java",
-	lazy = false,
-	opts = {
-		root_markers = {
-			"settings.gradle",
-			"settings.gradle.kts",
-			"pom.xml",
-			"build.gradle",
-			"mvnw",
-			"gradlew",
-			"build.gradle",
-			"build.gradle.kts",
-			".git",
-		},
-
-		jdtls = {
-			version = "v1.43.0",
-		},
-
-		lombok = {
-			enable = true,
-			version = "nightly",
-		},
-
-		-- load java test plugins
-		java_test = {
-			enable = true,
-			version = "0.40.1",
-		},
-
-		-- load java debugger plugins
-		java_debug_adapter = {
-			enable = true,
-			version = "0.58.1",
-		},
-
-		spring_boot_tools = {
-			enable = false,
-			version = "1.55.1",
-		},
-
-		notifications = {
-			-- enable 'Configuring DAP' & 'DAP configured' messages on start up
-			dap = false,
-		},
-	},
+	lazy = true,
+	filetype = "java",
+	config = false
 }
